5efa96ffe4989430dcab1d1da05cd26a9b1953d8
ChrisTheWeak <chris.gram.j@gmail.com>
2025-03-22 17:44:30 -0400

    Delete server.py

--- server.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, jsonify
-app= Flask(__name__)
-@app.route('/run-script',methods=['GET'])
-def run_script():
-    print(3.14*(80**2))
-    return jsonify({"message": "Python script executed"})
